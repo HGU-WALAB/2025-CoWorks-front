e263517ddc2a3c08859427dc596df6f8ada3bf6f
Sumin Jeon <wjstnals1211@gmail.com>
2025-09-26 12:32:01 +0900

    feat: 폴더 트리에서 문서 및 자식 폴더 개수 표시 기능 추가

--- src/components/FolderTree.tsx
+++ src/components/FolderTree.tsx
@@ -83,12 +83,28 @@ const FolderTreeNode: React.FC<FolderTreeNodeProps> = ({
           {folder.name}
         </span>
         
-        {/* 자식 폴더 개수 표시 */}
-        {folder.childrenCount > 0 && (
-          <span className="text-xs text-gray-400 ml-1">
-            ({folder.childrenCount})
-          </span>
-        )}
+        {/* 문서 및 자식 폴더 개수 표시 */}
+        <div className="flex items-center space-x-2 ml-2 text-xs text-gray-500">
+          {/* 문서 개수 */}
+          {folder.documentsCount > 0 && (
+            <div className="flex items-center bg-blue-100 text-blue-600 px-1.5 py-0.5 rounded-full">
+              <svg className="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
+                <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clipRule="evenodd" />
+              </svg>
+              <span>{folder.documentsCount}</span>
+            </div>
+          )}
+          
+          {/* 자식 폴더 개수 */}
+          {folder.childrenCount > 0 && (
+            <div className="flex items-center bg-yellow-100 text-yellow-600 px-1.5 py-0.5 rounded-full">
+              <svg className="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" />
+              </svg>
+              <span>{folder.childrenCount}</span>
+            </div>
+          )}
+        </div>
       </div>
       
       {/* 하위 폴더들 */}
